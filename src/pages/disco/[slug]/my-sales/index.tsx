373ec82e8e82239ccb2f05b3af9acb832eacbab3
Risbel Suarez <risbel961019@gmail.com>
2023-11-19 10:41:02 -0500

    feat: Add section pending in MySales page

--- src/pages/disco/[slug]/my-sales/index.tsx
+++ src/pages/disco/[slug]/my-sales/index.tsx
@@ -8,6 +8,8 @@ import { IReservationsByDiscoSlug } from "@/services/getReservationsByDiscoSlug"
 import { useParams } from "next/navigation";
 import React, { useState } from "react";
 import SearchReservationsForm from "./components/SearchReservationsBar";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 const MySales = () => {
   const days = useListDays();
@@ -56,28 +58,135 @@ const MySales = () => {
   return (
     <HomeLayout>
       <div className="pt-16 px-4 md:px-8">
-        <h1 className="text-white text-2xl mb-2 text-center md:text-start font-semibold">My Sales</h1>
+        <Link
+          className="absolute flex gap-2 text-white font-light border px-[1px] rounded-md hover:bg-white/10"
+          href={`/disco/${param?.slug}`}
+        >
+          <ChevronLeft width={20} />
+        </Link>
+        <h1 className="text-white text-2xl mb-2 text-center font-semibold">My Sales</h1>
 
         <SearchReservationsForm setSearchParams={setSearchParams} />
 
         <div className="flex justify-center">{isLoading && <Spinner diameter={8} />}</div>
 
+        <h1 className="text-white text-xl mb-4 text-center md:text-start">Sold to use today</h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {sortedReservations &&
+            sortedReservations.map((reservation) => {
+              if (
+                new Date().toLocaleDateString().slice(0, 10) ===
+                new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).toLocaleDateString().slice(0, 10)
+              ) {
+                return (
+                  <div key={reservation.id} className="p-2 border rounded-md bg-white/10">
+                    <div className="flex justify-around bg-black/30">
+                      <p className="text-white text-xs font-light ">Today</p>
+                      <p className="text-white text-xs font-light ">
+                        {days[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDay()]}-
+                        {new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDate()}-
+                        {months[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getMonth()]}
+                      </p>
+                    </div>
+
+                    <div className="flex justify-between">
+                      <p className="text-white">{reservation.User.name}</p>
+                      <p className="text-white font-light">{reservation.User.phone}</p>
+                    </div>
+                    <div>
+                      <p className="text-white">
+                        <span className="font-semibold bg-black/20">Invoice:</span> {reservation.id.slice(0, 13)}
+                      </p>
+                    </div>
+                    <div className="grid grid-flow-col gap-2">
+                      {reservation.ticketsReservations.map((ticket) => (
+                        <div className="py-2" key={ticket.id}>
+                          <div className="bg-black/20 p-2 rounded-md">
+                            <div className="flex justify-between items-center">
+                              <LogoCategory category={ticket.DiscoTicket.category} />
+
+                              <p className="text-white text-sm">
+                                {ticket.quantity} {ticket.DiscoTicket.category}{" "}
+                                {Number(ticket.quantity) > 1 ? "tickets" : "ticket"}
+                              </p>
+                              <p className="text-white text-sm">{ticket.DiscoTicket.price} c/u</p>
+                            </div>
+                            <p className="text-white text-xs font-light">{ticket.DiscoTicket.shortDescription}</p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                );
+              }
+            })}
+        </div>
+        <div className="border-b-[1px] border-dashed w-full my-8" />
+        <h1 className="text-white text-xl mb-4 text-center md:text-start">Pending</h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
+          {sortedReservations &&
+            sortedReservations.map((reservation) => {
+              if (
+                new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDate().valueOf() >
+                new Date().getDate()
+              ) {
+                return (
+                  <div key={reservation.id}>
+                    <div className="p-2 border rounded-md bg-white/10">
+                      <p className="text-white text-center text-xs font-light bg-black/30">
+                        {days[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDay()]}-
+                        {new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDate()}-
+                        {months[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getMonth()]}
+                      </p>
+
+                      <div className="flex justify-between">
+                        <p className="text-white">{reservation.User.name}</p>
+                        <p className="text-white font-light">{reservation.User.phone}</p>
+                      </div>
+                      <div>
+                        <p className="text-white">
+                          <span className="font-semibold bg-black/20">Invoice:</span> {reservation.id.slice(0, 13)}
+                        </p>
+                      </div>
+                      <div className="grid grid-flow-col gap-2">
+                        {reservation.ticketsReservations.map((ticket) => (
+                          <div className="py-2" key={ticket.id}>
+                            <div className="bg-black/20 p-2 rounded-md">
+                              <div className="flex justify-between items-center">
+                                <LogoCategory category={ticket.DiscoTicket.category} />
+
+                                <p className="text-white text-sm">
+                                  {ticket.quantity} {ticket.DiscoTicket.category}{" "}
+                                  {Number(ticket.quantity) > 1 ? "tickets" : "ticket"}
+                                </p>
+                                <p className="text-white text-sm">{ticket.DiscoTicket.price} c/u</p>
+                              </div>
+                              <p className="text-white text-xs font-light">{ticket.DiscoTicket.shortDescription}</p>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+        </div>
+        <div className="border-b-[1px] border-dashed w-full my-8" />
+        <h1 className="text-white text-xl mb-4 text-center md:text-start">Used yesterday</h1>
         {sortedReservations &&
           sortedReservations.map((reservation) => {
             if (
-              new Date().toLocaleDateString().slice(0, 10) ===
+              yesterday.toLocaleDateString().slice(0, 10) ===
               new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).toLocaleDateString().slice(0, 10)
             ) {
               return (
-                <div key={reservation.id}>
-                  <h1 className="text-white text-xl mb-4 text-center md:text-start underline underline-offset-2">
-                    Today
-                  </h1>
+                <div key={reservation.id} className="mb-16">
                   <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <div className="p-2 border rounded-md bg-white/10">
                       <div className="flex justify-around bg-black/30">
-                        <p className="text-white text-xs font-light ">Today</p>
-                        <p className="text-white text-xs font-light ">
+                        <p className="text-white text-xs font-light ">yesterday</p>
+                        <p className="text-white text-center text-xs font-light">
                           {days[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDay()]}-
                           {new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDate()}-
                           {months[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getMonth()]}
@@ -113,62 +222,8 @@ const MySales = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="border-b-[1px] border-dashed w-full my-8" />
                 </div>
               );
-            } else {
-              if (
-                yesterday.toLocaleDateString().slice(0, 10) ===
-                new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).toLocaleDateString().slice(0, 10)
-              ) {
-                return (
-                  <div key={reservation.id} className="mb-16">
-                    <h1 className="text-white text-xl mb-4 text-center md:text-start">
-                      Yesterday {days[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDay()]}-
-                      {new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDate()}-
-                      {months[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getMonth()]}
-                    </h1>
-
-                    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-                      <div className="p-2 border rounded-md bg-white/10">
-                        <p className="text-white text-center text-xs font-light bg-black/30">
-                          {days[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDay()]}-
-                          {new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getDate()}-
-                          {months[new Date(reservation.ticketsReservations[0].DiscoTicket.expDate).getMonth()]}
-                        </p>
-
-                        <div className="flex justify-between">
-                          <p className="text-white">{reservation.User.name}</p>
-                          <p className="text-white font-light">{reservation.User.phone}</p>
-                        </div>
-                        <div>
-                          <p className="text-white">
-                            <span className="font-semibold bg-black/20">Invoice:</span> {reservation.id.slice(0, 13)}
-                          </p>
-                        </div>
-                        <div className="grid grid-flow-col gap-2">
-                          {reservation.ticketsReservations.map((ticket) => (
-                            <div className="py-2" key={ticket.id}>
-                              <div className="bg-black/20 p-2 rounded-md">
-                                <div className="flex justify-between items-center">
-                                  <LogoCategory category={ticket.DiscoTicket.category} />
-
-                                  <p className="text-white text-sm">
-                                    {ticket.quantity} {ticket.DiscoTicket.category}{" "}
-                                    {Number(ticket.quantity) > 1 ? "tickets" : "ticket"}
-                                  </p>
-                                  <p className="text-white text-sm">{ticket.DiscoTicket.price} c/u</p>
-                                </div>
-                                <p className="text-white text-xs font-light">{ticket.DiscoTicket.shortDescription}</p>
-                              </div>
-                            </div>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              }
             }
           })}
       </div>
