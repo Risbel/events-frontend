c7420ec60ca22d9079fbc66b337cc586b5632e23
Risbel Suarez <risbel961019@gmail.com>
2024-01-07 00:07:52 -0500

    style: Restyle `/auth` pages

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,15 +1,28 @@
+import { cn } from "@/lib/shadcnUtils";
 import * as React from "react";
 
-import { cn } from "@/lib/shadcnUtils";
-
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Label = ({ name, htmlfor, className }: { name: string; htmlfor: string; className?: string }) => {
+  return (
+    <label
+      className={cn(
+        "absolute -top-[14px] md:-top-[16px] left-3 bg-white rounded-md text-black px-1 text-sm md:text-base",
+        className
+      )}
+      htmlFor={htmlfor}
+    >
+      {name}
+    </label>
+  );
+};
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
   return (
     <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-10 w-full rounded-md border border-black bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground outline-none disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       ref={ref}
@@ -19,4 +32,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
 });
 Input.displayName = "Input";
 
-export { Input };
+export { Input, Label };
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Input, Label } from "@/components/ui/input";
 import { useSubmitSignup } from "@/hooks/useSubmitSignup";
 import Link from "next/link";
 import Spinner from "@/components/loaders/Spinner";
 import AuthLayout from "@/components/layouts/AuthLayout.tsx";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import Image from "next/image";
 
 const Signup = () => {
+  const [isPassword, setIsPassword] = useState(false);
+  const [isRePassword, setIsRePassword] = useState(false);
   const [confirmAlert, setConfirmAlert] = useState(false);
   const [personalDates, setPersonalDates] = useState<SignupState>({
     name: "",
@@ -47,72 +51,110 @@ const Signup = () => {
 
   return (
     <AuthLayout>
-      <Link
-        className={`${buttonVariants({
-          variant: "outline",
-        })} `}
-        href={"/auth/login"}
-      >
-        Back To Login
-      </Link>
-      <p className="text-2xl font-bold text-white text-center py-4">Sign Up</p>
-      <form
-        onSubmit={handleSubmit}
-        name="signup form"
-        className="flex flex-col gap-4 border p-4  bg-slate-200/10 backdrop-blur-2xl"
-      >
-        <Input onChange={handleChange} value={personalDates.name} name="name" type="text" placeholder="name" />
-        <Input
-          required
-          onChange={handleChange}
-          value={personalDates.lastName}
-          name="lastName"
-          type="text"
-          placeholder="last name"
-        />
-        <Input onChange={handleChange} value={personalDates.email} name="email" type="email" placeholder="email" />
-        <Input
-          required
-          onChange={handleChange}
-          value={personalDates.phone}
-          name="phone"
-          type="text"
-          placeholder="phone number"
-        />
-        <Input
-          autoComplete="off"
-          required
-          onChange={handleChange}
-          value={personalDates.password}
-          name="password"
-          type="password"
-          placeholder="password"
-        />
-
-        <Input
-          autoComplete="off"
-          required
-          onChange={handleChange}
-          value={personalDates.confirmPassword}
-          name="confirmPassword"
-          type="password"
-          placeholder="confirm password"
-        />
-
-        {confirmAlert && <span className="text-red-600">Rectify the confirmation</span>}
-
-        <Button type="submit">Sign up</Button>
-        {isLoading && (
-          <div className="flex w-full justify-center">
-            <Spinner diameter={4} />
+      <div className="px-4">
+        <Link
+          className={`${buttonVariants({
+            variant: "outline",
+          })} absolute left-6 top-5`}
+          href={"/auth/login"}
+        >
+          Back To Login
+        </Link>
+        <p className="text-2xl font-bold text-start py-4">SIGN UP</p>
+        <form onSubmit={handleSubmit} name="signup form" className="flex flex-col gap-4">
+          <div className="relative">
+            <Label name={"Name"} htmlfor={"name"} />
+            <Input onChange={handleChange} value={personalDates.name} name="name" type="text" placeholder="name" />
           </div>
-        )}
-        {isSuccess && (
-          <div className="flex justify-center w-full">
-            <span className="text-green-600">Successful</span>
+          <div className="relative">
+            <Label name={"Last Name"} htmlfor={"lastName"} />
+            <Input
+              required
+              onChange={handleChange}
+              value={personalDates.lastName}
+              name="lastName"
+              type="text"
+              placeholder="last name"
+            />
           </div>
-        )}
-      </form>
+          <div className="relative">
+            <Label name={"Email"} htmlfor={"email"} />
+            <Input onChange={handleChange} value={personalDates.email} name="email" type="email" placeholder="email" />
+          </div>
+          <div className="relative">
+            <Label name={"Phone"} htmlfor={"phone"} />
+            <Input
+              required
+              onChange={handleChange}
+              value={personalDates.phone}
+              name="phone"
+              type="text"
+              placeholder="phone number"
+            />
+          </div>
+          <div className="relative">
+            <Label name={"Password"} htmlfor={"password"} />
+            <Input
+              autoComplete="off"
+              required
+              onChange={handleChange}
+              value={personalDates.password}
+              name="password"
+              type={isPassword ? "text" : "password"}
+              placeholder="password"
+            />
+            <button
+              type="button"
+              className="absolute right-2 top-2 cursor-pointer"
+              onClick={() => setIsPassword((prev) => !prev)}
+            >
+              {isPassword ? <EyeIcon className="stroke-gray-700" /> : <EyeOffIcon className="stroke-gray-700" />}
+            </button>
+          </div>
+          <div className="relative">
+            <Label name={"Confirm Password"} htmlfor={"password"} />
+            <Input
+              autoComplete="off"
+              required
+              onChange={handleChange}
+              value={personalDates.confirmPassword}
+              name="confirmPassword"
+              type={isRePassword ? "text" : "password"}
+              placeholder="confirm password"
+            />
+            <button
+              type="button"
+              className="absolute right-2 top-2 cursor-pointer"
+              onClick={() => setIsRePassword((prev) => !prev)}
+            >
+              {isRePassword ? <EyeIcon className="stroke-gray-700" /> : <EyeOffIcon className="stroke-gray-700" />}
+            </button>
+          </div>
+
+          {confirmAlert && <span className="text-red-600">Rectify the confirmation</span>}
+
+          <Button className="flex gap-2" type="submit">
+            Sign up {isLoading && <Spinner diameter={4} />}
+          </Button>
+
+          {isSuccess && (
+            <div className="flex justify-center w-full">
+              <span className="text-green-600">Successful</span>
+            </div>
+          )}
+        </form>
+      </div>
+      <div className="relative hidden md:flex flex-col text-gray-700">
+        <h2 className="absolute left-0 right-0 text-white text-4xl font-bold">My Events</h2>
+        <Image
+          priority
+          className="w-auto"
+          src={"/image-auth-hands-tickets.jpg"}
+          height={400}
+          width={400}
+          alt=" image auth hands tickets"
+        />
+      </div>
     </AuthLayout>
   );
 };
