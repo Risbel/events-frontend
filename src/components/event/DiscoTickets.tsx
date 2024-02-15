6a04e52425689d5bc6d118babbf3c6639c55a220
Risbel Suarez <risbel961019@gmail.com>
2024-02-14 19:36:48 -0500

    style: Add styles in `Tickets` section

--- src/components/event/DiscoTickets.tsx
+++ src/components/event/DiscoTickets.tsx
@@ -126,137 +126,128 @@ const DiscoTickets = ({
 
   return (
     <div style={{ background: `${discoDetail.discoColor.bgTicketsSection}` }}>
-      {discoTickets?.length !== 0 && (
-        <>
-          <h1
-            style={{ color: `${discoDetail.discoColor.ticketH1Color}` }}
-            className="font-extrabold text-4xl md:text-5xl lg:text-7xl text-center pb-2 pt-20"
-          >
-            Tickets
-          </h1>
-          <div className="flex justify-center pb-4">
-            <div
-              style={{
-                background: `${discoDetail.discoColor.ticketH1Color}99`,
-                border: `solid 3px`,
-                borderColor: `${discoDetail.discoColor.ticketH1Color}`,
-              }}
-              className="flex gap-1 overflow-hidden max-w-screen-lg overflow-x-auto rounded-md p-2"
-            >
-              {unicDates.map((date, i) => {
-                if (compareAsc(new Date(new Date(date).toDateString()), new Date(new Date().toDateString())) >= 0) {
-                  return (
-                    <button
-                      key={i}
-                      style={{ background: `${discoDetail.discoColor.buttonsTicketsColor}` }}
-                      className="flex flex-col items-center px-4 py-2 hover:opacity-90 leading-none rounded-md hover:-translate-y-[2px] transition-transform"
-                      onClick={() => setDay(date)}
-                    >
-                      <p
-                        style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}
-                        className="text-xs font-semibold"
-                      >
-                        {weekdays[new Date(date).getDay()].slice(0, 3)}
-                      </p>
-                      <p
-                        style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}
-                        className="text-xl font-bold"
-                      >
-                        {new Date(date).getDate()}
-                      </p>
-                      <p
-                        style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}
-                        className="text-xs font-semibold"
-                      >
-                        {months[new Date(date).getMonth()].slice(0, 3)}
-                      </p>
-                    </button>
-                  );
-                }
-              })}
-            </div>
-          </div>
-          <div className="flex flex-col items-center gap-4 mb-10 p-2 rounded-md lg:px-8">
-            {discoTickets?.map((ticket) => {
-              if (new Date(ticket.expDate).toDateString() === day) {
-                return (
-                  <div key={ticket.id} className="w-full md:w-1/2 lg:w-2/3">
-                    <div className="relative">
-                      {Number(ticket.countInStock) === 0 ? (
-                        <div className="absolute z-20 w-full h-full bg-gray-800/80 border border-white rounded-md flex items-center justify-center">
-                          <p className="text-slate-200 text-2xl">Sold out</p>
-                        </div>
-                      ) : (
-                        ticket.ticketsReservations.length >= 1 &&
-                        ticket.category !== "common" && (
-                          <div className="absolute z-20 w-full h-full bg-gray-800/80 border border-white rounded-md flex items-center justify-center">
-                            <p className="text-slate-200 text-2xl">Reserved</p>
-                          </div>
-                        )
-                      )}
-                      <Link
-                        style={{
-                          background: `${discoDetail.discoColor.buttonsTicketsColor}`,
-                          border: `solid ${discoDetail.discoColor.buttonTicketForeground} 2px`,
-                        }}
-                        href={`/event/${name}/details-ticket/${ticket.id}`}
-                        className="flex justify-between gap-2 rounded-md p-2 relative hover:shadow-xl hover:-translate-y-1 transition-transform duration-300"
-                      >
-                        <div style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }} className="w-full">
-                          <p className="text-sm font-semibold">Reserve {ticket.category} tickets</p>
-                          <div className="flex gap-3 items-center">
-                            <div className="flex items-center gap-2">
-                              <CreditCard style={{ stroke: `${discoDetail.discoColor.buttonTicketForeground}` }} />
-                              <div className="text-sm"> ${ticket.price} c/u</div>
-                            </div>
-                            {(ticket.category === "VIP" || ticket.category === "economy") && (
-                              <div className="flex items-center gap-2">
-                                <RockingChair stroke={`${discoDetail.discoColor.buttonTicketForeground}`} />
-                                <p className="text-xs">{ticket.countInStock}available</p>
-                              </div>
-                            )}
-                          </div>
-                          <div style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}>
-                            <p className="text-xs"> {ticket?.shortDescription}</p>
-                            <div className="w-full flex items-center justify-center gap-2">
-                              <CalendarDays style={{ stroke: `${discoDetail.discoColor.buttonTicketForeground}` }} />
-                              <p className="text-xs text-center">
-                                {weekdays[new Date(ticket.expDate).getDay()]} {new Date(ticket.expDate).getDate()}
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex absolute items-center gap-1 md:gap-2 right-1">
-                          <div>{ticket.countInStock}</div>
-                          <LogoCategory ticket={ticket} discoColors={discoDetail.discoColor} />
-                        </div>
-                      </Link>
+      <h1
+        style={{ color: `${discoDetail.discoColor.ticketH1Color}` }}
+        className="font-extrabold text-4xl md:text-5xl lg:text-7xl text-center mb-8 pt-20"
+      >
+        Tickets
+      </h1>
+      <div className="flex justify-center mb-4">
+        <div
+          style={{
+            background: `${discoDetail.discoColor.buttonTicketForeground}90`,
+            border: `solid ${discoDetail.discoColor.buttonTicketForeground} 4px`,
+          }}
+          className="flex gap-1 overflow-hidden max-w-screen-lg overflow-x-auto rounded-xl p-2"
+        >
+          {unicDates.map((date, i) => {
+            if (compareAsc(new Date(new Date(date).toDateString()), new Date(new Date().toDateString())) >= 0) {
+              return (
+                <button
+                  key={i}
+                  style={{ background: `${discoDetail.discoColor.buttonsTicketsColor}` }}
+                  className="flex flex-col items-center px-4 py-2 hover:opacity-90 leading-none rounded-md hover:-translate-y-[2px] transition-transform"
+                  onClick={() => setDay(date)}
+                >
+                  <p
+                    style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}
+                    className="text-xs font-semibold"
+                  >
+                    {weekdays[new Date(date).getDay()].slice(0, 3)}
+                  </p>
+                  <p
+                    style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}
+                    className="text-xl font-bold"
+                  >
+                    {new Date(date).getDate()}
+                  </p>
+                  <p
+                    style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}
+                    className="text-xs font-semibold"
+                  >
+                    {months[new Date(date).getMonth()].slice(0, 3)}
+                  </p>
+                </button>
+              );
+            }
+          })}
+        </div>
+      </div>
+
+      <div className="flex justify-center max-w-screen flex-wrap gap-4 mb-10 p-2 rounded-md px-8">
+        {discoTickets?.map((ticket) => {
+          if (new Date(ticket.expDate).toDateString() === day) {
+            return (
+              <div key={ticket.id} className="relative w-full md:w-1/3 lg:w-1/5 min-w-64">
+                {Number(ticket.countInStock) === 0 ? (
+                  <div className="absolute z-20 w-full h-full bg-gray-800/80 border border-white rounded-3xl flex items-center justify-center">
+                    <p className="text-slate-200 text-2xl">Sold out</p>
+                  </div>
+                ) : (
+                  ticket.ticketsReservations.length >= 1 &&
+                  ticket.category !== "common" && (
+                    <div className="absolute z-20 w-full h-full bg-gray-800/80 border border-white rounded-md flex items-center justify-center">
+                      <p className="text-slate-200 text-2xl">Reserved</p>
                     </div>
+                  )
+                )}
+                <div
+                  style={{
+                    background: `${discoDetail.discoColor.buttonsTicketsColor}`,
+                    border: `solid ${discoDetail.discoColor.buttonTicketForeground} 4px`,
+                    color: `${discoDetail.discoColor.buttonTicketForeground}`,
+                  }}
+                  className="flex flex-col h-full gap-2 justify-between items-center rounded-3xl py-8 relative"
+                >
+                  <p
+                    style={{ border: `solid ${discoDetail.discoColor.buttonTicketForeground}90 1px` }}
+                    className="text-center text-xl font-bold mb-2 border px-2 rounded-full"
+                  >
+                    {ticket.category}
+                  </p>
 
-                    <div className="flex gap-4 my-2">
-                      {havePermission("update", "Tickets") &&
-                        (ticket.ticketsReservations.length < 1 || ticket.category === "common") && (
-                          <div>
-                            <EditTicketsForm
-                              id={ticket.id}
-                              price={ticket.price}
-                              countInStock={ticket.countInStock}
-                              shortDescription={ticket.shortDescription}
-                            />
-                          </div>
-                        )}
+                  <div className="flex items-center gap-2 pb-4">
+                    <div className="text-5xl font-bold"> ${ticket.price}</div>
+                  </div>
 
-                      {ticket.ticketsReservations.length < 1 && havePermission("delete", "Tickets") && (
-                        <DeleteTicketButton id={ticket.id} />
-                      )}
+                  {(ticket.category === "VIP" || ticket.category === "economy") && (
+                    <div className="flex items-center">
+                      <p className="font-semibold">{ticket.countInStock} seats available</p>
+                    </div>
+                  )}
+                  <p className="text-center text-xs pb-6 w-2/3"> {ticket?.shortDescription}</p>
+
+                  <div className="flex absolute right-1 top-1 items-center gap-1 md:gap-2">
+                    <LogoCategory ticket={ticket} discoColors={discoDetail.discoColor} />
+                  </div>
+
+                  <Link
+                    className="text-center font-semibold py-1 px-4 rounded-md hover:-translate-y-1 shadow-lg transition-transform duration-300"
+                    style={{
+                      background: `${discoDetail.discoColor.buttonsTicketsColor}`,
+                      border: `solid ${discoDetail.discoColor.buttonTicketForeground} 1px`,
+                      color: `${discoDetail.discoColor.buttonTicketForeground}`,
+                    }}
+                    href={`/event/${name}/details-ticket/${ticket.id}`}
+                  >
+                    Get tickets
+                  </Link>
+                  <p>{ticket.largeDescription}</p>
+                  <div style={{ color: `${discoDetail.discoColor.buttonTicketForeground}` }}>
+                    <div className="font-semibold w-full flex items-center justify-center gap-2">
+                      <p className="text-xs text-center">
+                        {weekdays[new Date(ticket.expDate).getDay()]} {months[new Date(ticket.expDate).getMonth()]}{" "}
+                        {new Date(ticket.expDate).getDate()}
+                      </p>
                     </div>
                   </div>
-                );
-              }
-            })}
-          </div>
-        </>
-      )}
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+
       <div
         className={clsx(
           !havePermission("create", "Tickets") && "hidden",
@@ -272,3 +263,21 @@ const DiscoTickets = ({
 };
 
 export default DiscoTickets;
+
+{
+  /* {havePermission("update", "Tickets") &&
+                      (ticket.ticketsReservations.length < 1 || ticket.category === "common") && (
+                        <div>
+                          <EditTicketsForm
+                            id={ticket.id}
+                            price={ticket.price}
+                            countInStock={ticket.countInStock}
+                            shortDescription={ticket.shortDescription}
+                          />
+                        </div>
+                      )}
+
+                    {ticket.ticketsReservations.length < 1 && havePermission("delete", "Tickets") && (
+                      <DeleteTicketButton id={ticket.id} />
+                    )} */
+}
