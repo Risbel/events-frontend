7c638a826292c8a7a6cb043b555e262033dfb133
Risbel Suarez <risbel961019@gmail.com>
2024-04-30 12:00:13 -0400

    fix: Fix link in allevents section

--- src/pages/dashboard/allevents/components/TargetEvent.tsx
+++ src/pages/dashboard/allevents/components/TargetEvent.tsx
@@ -33,7 +33,7 @@ const TargetEvent = ({ event }: { event: DataDisco }) => {
 
         <div className="flex px-2 md:px-4 justify-end">
           <a
-            href={`https://my-events-pi.vercel.app/event/${event?.slug}`}
+            href={`https://event.myaipeople.com/event/${event?.slug}`}
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center bg-gray-200 hover:bg-gray-200/90 px-4 py-0.5 md:py-1 rounded-xl"
